688d40edfc262c4feb5b6afbb6b27fc95c1f6526
Anita O <anita.orstad@gmail.com>
2022-11-10 14:30:51 +0100

    first commit

---
+++ src/test/java/com/annaprogramming/blog/AnnaProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annaprogramming.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnaProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
