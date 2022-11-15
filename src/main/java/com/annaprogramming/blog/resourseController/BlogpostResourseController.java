44a673acd3b42407e96256a84566deca51e4b98a
Anita O <anita.orstad@gmail.com>
2022-11-15 11:58:55 +0100

    Added controller

--- src/main/java/com/annaprogramming/blog/resourseController/BlogpostResourseController.java
+++ src/main/java/com/annaprogramming/blog/resourseController/BlogpostResourseController.java
@@ -1,2 +1,9 @@
-package com.annaprogramming.blog.resourseController;public class BlogpostResourseController {
+package com.annaprogramming.blog.resourseController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/server")
+public class BlogpostResourseController {
 }
