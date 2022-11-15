5156d799d3c6af91c33966eff50d16c6bd389e50
Anita O <anita.orstad@gmail.com>
2022-11-15 11:44:41 +0100

    added BlogpostServiceImpl with methodes returning null

---
+++ src/main/java/com/annaprogramming/blog/service/implementation/BlogpostServiceImpl.java
@@ -0,0 +1,34 @@
+package com.annaprogramming.blog.service.implementation;
+
+import com.annaprogramming.blog.model.Blogpost;
+import com.annaprogramming.blog.service.BlogpostService;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class BlogpostServiceImpl implements BlogpostService {
+    @Override
+    public Blogpost create(Blogpost blogpost) {
+        return null;
+    }
+
+    @Override
+    public Collection<Blogpost> list() {
+        return null;
+    }
+
+    @Override
+    public Blogpost get(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Blogpost update(Blogpost blogpost) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(UUID id) {
+        return null;
+    }
+}
