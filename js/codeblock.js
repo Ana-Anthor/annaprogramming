df25e6c3b723fd7e00f607c3e681612f5e3ef067
Anita Orstad <anita.orstad@gmail.com>
2026-04-12 20:16:11 +0200

    added checkmark to copy code btn

--- js/codeblock.js
+++ js/codeblock.js
@@ -1,18 +1,20 @@
+const copyIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"/></svg>`;
+
+const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5"/></svg>`;
+
 class CodeBlock extends HTMLElement {
   connectedCallback() {
 
     const text = this.textContent.trim();
-
     this.innerHTML = `
-      <div class="code-wrapper">
-        <pre><button class="copy-btn"><svg><use href="/icons/icons.svg#copy"></use></svg></button><code>${this.toHtml(text)}</code></pre>
-      </div>
-    `;
+        <pre><button class="copy-btn">${copyIcon}</button><code>${this.toHtml(text)}</code></pre>`;
 
     this.querySelector('.copy-btn').addEventListener('click', () => {
-      navigator.clipboard.writeText(text).then(() => {
-        const btn = this.querySelector('.copy-btn');
-      });
+        navigator.clipboard.writeText(text).then(() => {
+            const btn = this.querySelector('.copy-btn');
+            btn.innerHTML = checkIcon;
+            setTimeout(() => btn.innerHTML = copyIcon, 2000);
+        });
     });
   }
 
